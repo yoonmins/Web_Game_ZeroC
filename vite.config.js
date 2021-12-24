addcc27aef1f9cf29e088566331715b370a93d70
SungMin Yoon <89017779+Y00NMIN@users.noreply.github.com>
2021-12-24 11:58:35 +0000

    js

--- vite.config.js
+++ vite.config.js
@@ -13,3 +13,19 @@ export default {
     }
   }
 }
+
+<script type="text/babel">
+
+class LikeButton extends React.Component{
+		constructor(props){
+			super(props);
+				this.state = {
+					liked : false,
+					};
+				}
+				
+				render(){
+					return <button type="submite" onClick= {() => {this.setState({liked : true})}>{this.state.liked === true ? 'Liked' : 'Like'}</button>;
+				}
+			}
+</script>
